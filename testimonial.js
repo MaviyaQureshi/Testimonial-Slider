7fd4856fea67582f3dbd8e71014c0b73cea13ca9
MaviyaQureshi <maviyaqureshi2003@gmail.com>
2022-09-18 14:12:55 +0530

    first commit

--- testimonial.js
+++ testimonial.js
@@ -1,38 +1,42 @@
 const testimonials = [
     {
-        name: "Cherise G",
-        photoUrl : "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-        text: "I couldn't have asked for more than this. Apple is worth much more than I paid. Apple did exactly what you said it does. I will let my mum know about this, she could really make use of Apple!",
+      name: "Cherise G",
+      photoUrl:
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+      text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
     },
     {
-        name: "Rosette Q",
-        photoUrl : "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-        text : "I don't know what else to say. I love your system. Apple was worth a fortune to my company. Thank you for making it painless, pleasant and most of all hassle free!",
+      name: "Rosetta Q",
+      photoUrl:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+      text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
     },
     {
-        name : "Jordana Q",
-        photoUrl :"https://images.unsplash.com/photo-1640951613773-54706e06851d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-        text : "Since I invested in Apple I made over 100,000 dollars profits. I would be lost without Apple. This is simply unbelievable!",
+      name: "Constantine V",
+      photoUrl:
+        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+      text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
     },
-];
-
-imgEl = document.querySelector("img");
-const textEl = document.querySelector(".text");
-const usernameEl = document.querySelector(".username");
-
-
-let idx = 0;
-
-function updateTestimonial(){
-    const{name,photoUrl,text}=testimonials[idx]
-    imgEli.src = photoUrl
-    textEl.innerText = text
+  ];
+  
+  const imgEl = document.querySelector("img");
+  const textEl = document.querySelector(".text");
+  const usernameEl = document.querySelector(".username");
+  
+  let idx = 0;
+  
+  updateTestimonial();
+  
+  function updateTestimonial() {
+    const { name, photoUrl, text } = testimonials[idx];
+    imgEl.src = photoUrl;
+    textEl.innerText = text;
     usernameEl.innerText = name;
-    idx++
-    if(idx === testimonial.length){
-        idx=0;
+    idx++;
+    if (idx === testimonials.length) {
+      idx = 0;
     }
-    setTimeout(()=>{
-        updateTestimonial();
-    }, 10000)
-}
+    setTimeout(() => {
+      updateTestimonial();
+    }, 6000);
+  }
